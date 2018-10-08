a2307422cd005788136753912357db0166d735df
casey bradford <casey.bradford@wework.com>
2018-10-08 00:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(name){
+  drivers.pop(name)
+}
+
+function destructivelyRemoveFirstDriver(name){
+  drivers.shift(name)
+}
+
+function appendDriver(name){
+ const arr2 = [...drivers, name];
+ return arr2
+}
+
+function prependDriver(name){
+  const arr3 = [name, ...drivers];
+ return arr3
+}
+
+
+function removeFirstDriver(){
+  const arr4 = drivers.splice(0, 1);
+  return arr4;
+}
+
